cbdb0b92d9eb0e8c355845a018090c91ea075f66
Marjona Rakhmatillaeva <70479211+Marjonagit@users.noreply.github.com>
2023-11-13 20:04:20 +0500

    Update and rename main.dart to main1.dart

--- lib/main1.dart
+++ lib/main1.dart
@@ -25,24 +25,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//greeting_widget.dart
-
-import 'package:flutter/material.dart';
-
-class GreetingWidget extends StatelessWidget {
-  final String greeting;
-
-  GreetingWidget(this.greeting);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(greeting);
-  }
-}
-
-
-
-//stateful widgets
-//counter
-//main.dart
 
